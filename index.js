d52b31a4d2737145a17adc5915d0876f835dac1f
Lorenzo Martinoli <lo.martinoli@gmail.com>
2020-11-27 14:17:32 +0100

    a barebone working backend that pushes usernames and passwords ina real database

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require("express");
-const app = express();
-var cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cors = require("cors");
 const port = 3001;
 const bodyParser = require("body-parser");
+const User = require("./user");
 
-app.use(cors());
+const app = express();
+
+mongoose.connect("mongodb+srv://Sbodazo:admin@cluster0.2q3gi.mongodb.net/Users?retryWrites=true&w=majority",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},
+() => {
+  console.log("Database connesso");
+});
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: "trip2day",
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(cookieParser("trip2day"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
 
 const accounts = [
   { nickname: "nickname", password: "password" },
@@ -64,21 +96,35 @@ const planner = [
   },
 ];
 
-app
-  .route("/auth")
-  .post((req, res) => {
-    let response = accounts.some((account) => {
-      return (
-        account.nickname === req.body.nickname &&
-        account.password === req.body.password
-      );
-    });
+app.post("/auth", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
 
-    res.json({ authenticated: response });
-  })
-  .all((req, res) => {
-    res.json({ error: "Unknown Method" });
+app.post("/register", (req, res) => {
+  User.findOne({username: req.body.username}, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const newUser = new User ({
+        username: req.body.username,
+        password: hashedPassword
+      });
+      await newUser.save();
+      res.send("User Created");
+    }
   });
+});
 
 app
   .route("/:nickname/planner/:where")
